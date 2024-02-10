f8d88e7d3f5a88116356dd5296340cb705703dc7
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-10 15:05:21 +0900

    Test NewEnclosing class

---
+++ src/test/java/com/hyungjunn/nested_class/NewEnclosing.java
@@ -0,0 +1,31 @@
+package com.hyungjunn.nested_class;
+
+import org.junit.jupiter.api.Test;
+
+public class NewEnclosing {
+
+    int x = 1;
+
+    void run() {
+
+        int x = 2;
+        class Local { // Local 클래스는 modifier를 쓸 수 없다
+
+            int x = 3;
+            void run() {
+                // method implementation
+                System.out.println(x);
+            }
+        }
+
+        Local local = new Local();
+        local.run();
+    }
+
+    @Test
+    void test() {
+        NewEnclosing newEnclosing = new NewEnclosing();
+        newEnclosing.run();
+    }
+}
+
