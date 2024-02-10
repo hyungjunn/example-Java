f01b3675dd4af1eb37a88436b098102ff0330a87
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-10 13:38:17 +0900

    Refactor StaticNestedClass Test

--- src/test/java/com/hyungjunn/nested_class/Enclosing.java
+++ src/test/java/com/hyungjunn/nested_class/Enclosing.java
@@ -1,5 +1,6 @@
 package com.hyungjunn.nested_class;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Enclosing {
@@ -8,17 +9,17 @@ public class Enclosing {
 
     public static class StaticNested {
 
-        private void run() {
+        private int run() {
             // method implementation
+            return x + 1;
         }
 
     }
 
     @Test
     void test() {
-
         Enclosing.StaticNested staticNested = new Enclosing.StaticNested();
-        staticNested.run();
+        Assertions.assertThat(staticNested.run()).isEqualTo(2);
     }
 
 }
