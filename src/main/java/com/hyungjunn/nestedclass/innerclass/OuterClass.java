df87172cbce21e9ca04f4ed9fd0d1d44bbf18097
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-13 18:58:49 +0900

    add Animal-running()

--- src/main/java/com/hyungjunn/nestedclass/innerclass/OuterClass.java
+++ src/main/java/com/hyungjunn/nestedclass/innerclass/OuterClass.java
@@ -4,6 +4,8 @@ public class OuterClass {
 
     interface Animal {
         public void crying();
+
+        public void running();
     }
 
     // innerclass
@@ -21,10 +23,17 @@ public class OuterClass {
 
         // localclass
         class Cat implements Animal{
+            private int speed = 10;
             private static final String cry = "MIAOW~~~ in localclass";
             @Override
             public void crying() {
                 System.out.println(cry);
+            }
+
+            @Override
+            public void running() {
+                speed += 10;
+                System.out.println("cat's speed: " + speed);
             }
         }
         Cat cat = new Cat();
@@ -32,15 +41,22 @@ public class OuterClass {
 
         Animal dog = new Animal() {
 
+            private int speed = 10;
             private static final String cry = "Baw Baw!!! in Anonymousclass";
 
             @Override
             public void crying() {
                 System.out.println(cry);
             }
+
+            @Override
+            public void running() {
+                speed += 10;
+                System.out.println("dog's speed: " + speed);
+            }
         };
         dog.crying();
-
+        dog.running();
     }
 
     public static void main(String[] args) {
@@ -48,7 +64,7 @@ public class OuterClass {
         OuterClass outerClass = new OuterClass();
         OuterClass.InnerClass cat = outerClass.new InnerClass();
         cat.crying();
-        outerClass.behave();
+        outerClass.behave(); // 보일러플레이트 코드에서 메서드가 한개보다 많아질경우에 명확성이 떨어지게 된다
     }
 
 
