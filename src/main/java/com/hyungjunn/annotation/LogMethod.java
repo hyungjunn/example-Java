2f59625690a511c0dc4367412add770de8494bf0
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-18 09:49:27 +0900

    add LogMethod annotation

---
+++ src/main/java/com/hyungjunn/annotation/LogMethod.java
@@ -0,0 +1,13 @@
+package com.hyungjunn.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LogMethod {
+    boolean entry() default true;
+    boolean exit() default true;
+}
