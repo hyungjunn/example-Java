0dcd2f252317c891a5133aab68c4399481c9f41d
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-19 19:12:25 +0900

    add GenerateCode with @ClassPreamble

--- src/main/java/com/hyungjunn/annotation/AnnotationTest.java
+++ src/main/java/com/hyungjunn/annotation/AnnotationTest.java
@@ -17,3 +17,15 @@ public class AnnotationTest {
 
     }
 }
+
+@ClassPreamble(
+        author = "제임스 고슬링",
+        date = "02/10/2024",
+        currentRevision = 1,
+        lastModified = "02/19/2024",
+        lastModifiedBy = "조슈아 블로크",
+        reviewers = {"빌조이", "브라이언 고츠", "아서 반 호프"}
+)
+class GenerateCode {
+
+}
