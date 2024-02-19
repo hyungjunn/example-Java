7e57f7c254a88b9f8d03937232ae1ae9ddc41399
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-19 19:11:41 +0900

    add APrime.java

---
+++ src/main/java/com/hyungjunn/annotation/APrime.java
@@ -0,0 +1,13 @@
+package com.hyungjunn.annotation;
+
+public class APrime{
+
+    static void methodFrist() {
+        System.out.println("This is first method");
+    }
+
+    public static void main(String[] args) {
+        APrime.methodFrist();
+    }
+
+}
